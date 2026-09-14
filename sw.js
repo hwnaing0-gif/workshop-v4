ec3cbaf4ca28017a6753b062efd011e6c51a1ea7
hwnaing0-gif <hwnaing0@gmail.com>
2026-09-14 15:50:10 +0630

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ko-kyat-v5";
+const CACHE="ko-kyat-v6";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
